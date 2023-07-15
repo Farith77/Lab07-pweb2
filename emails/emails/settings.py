c8952804d78884c7c4f9f21b3660b85a1588602d
Farith <agarciaa@unsa.edu.pe>
2023-07-15 01:58:42 -0500

    settings y url - emails

--- emails/emails/settings.py
+++ emails/emails/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'send',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_HOST = ’smtp.gmail.com’
+EMAIL_PORT = 587
+EMAIL_HOST_USER =’farit7garcia7apaza@gmail.com’
+EMAIL_HOST_PASSWORD = ’mundomagico2.0’
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
